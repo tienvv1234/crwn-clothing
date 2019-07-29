9c6cbf09bfacf04fd9c533591059a7343b6c1817
tienvv1234 <tienvv1234@gmail.com>
2019-07-29 22:41:31 +0700

    update

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './homepage.styles.scss';
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+          <h1 className='subtitle'>SHOP NOW</h1>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Jackets</h1>
+          <h1 className='subtitle'>SHOP NOW</h1>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Sneakers</h1>
+          <h1 className='subtitle'>SHOP NOW</h1>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Womens</h1>
+          <h1 className='subtitle'>SHOP NOW</h1>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Mens</h1>
+          <h1 className='subtitle'>SHOP NOW</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
