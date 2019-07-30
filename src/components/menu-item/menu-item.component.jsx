a6d57c808ef968b97c7966fb4a2f45d5609a9b75
tienvv1234 <tienvv1234@gmail.com>
2019-07-30 23:05:38 +0700

    update

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className='background-image'
       style={{
@@ -15,4 +19,6 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+// withRouter is high component, it's take component is argument and return component with some more feature
+// withRouter(MenuItem) the MenuItem can access to history now
+export default withRouter(MenuItem);
