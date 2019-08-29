8b37d3febe97447279c682aa26fa2e5f5af163de
tienvv1234 <tienvv1234@gmail.com>
2019-08-30 00:20:07 +0700

    update

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,23 +5,23 @@ import CollectionItem from '../../components/collection-item/collection-item-com
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
-    const { title, items } = collection;
-    return (
-        <div className='collection-page'>
-            <h2 className='title'>{title}</h2>
-            <div className='items'>
-                {items.map(item => (
-                    <CollectionItem key={items.id} item={item} />
-                ))}
-            </div>
-        </div>
-    );
+  const { title, items } = collection;
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state, ownProps) => {
-    return {
-        collection: selectCollection(ownProps.match.params.collectionId)(state)
-    };
+  return {
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+  };
 };
 
 export default connect(mapStateToProps)(CollectionPage);
